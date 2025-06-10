cb45f99b3e3c8156d290fc86fb60c42974587c3f
Priyanshu Kumar <priyanshukumarkota@gmail.com>
2025-06-10 12:57:07 +0530

    Leet Code Problem No. 39 , find patterns to reach target using Recursion

--- LeetCode/Combination_Sum_39.java
+++ LeetCode/Combination_Sum_39.java
@@ -2,33 +2,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Combination_Sum_39 {
-  public static void findCombinations(int index, int[] candidates, int target, List<List<Integer>> result,
-      List<Integer> ds) {
-    if (index == candidates.length) {
+  public static void findCombination(int[] arr, int target, int index, List<List<Integer>> result, List<Integer> ds) {
+    if (index >= arr.length) {
       if (target == 0)
         result.add(new ArrayList<>(ds));
       return;
     }
-
-    if (candidates[index] <= target) {
-      ds.add(candidates[index]);
-      findCombinations(index, candidates, target - candidates[index], result, ds);
+    if (arr[index] <= target) {
+      ds.add(arr[index]);
+      findCombination(arr, target - arr[index], index, result, ds);
       ds.remove(ds.size() - 1);
     }
-
-    findCombinations(index + 1, candidates, target, result, ds);
+    findCombination(arr, target, index + 1, result, ds);
   }
 
-  public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+  public static List<List<Integer>> combinationSum(int[] arr, int target) {
     List<List<Integer>> result = new ArrayList<>();
-    findCombinations(0, candidates, target, result, new ArrayList<>());
+    findCombination(arr, target, 0, result, new ArrayList<>());
     return result;
   }
 
   public static void main(String[] args) {
-    int[] candidates = { 2, 3, 5 };
+    int[] arr = { 2, 3, 5 };
     int target = 8;
-    List<List<Integer>> combinations = combinationSum(candidates, target);
-    System.out.println("Combinations summing to " + target + ": " + combinations);
+    List<List<Integer>> result = combinationSum(arr, target);
+    System.out.println(result);
+
   }
 }
