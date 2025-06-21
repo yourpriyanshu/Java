edacd784f6dacc4b62c8f3ad75f8602f6636fc08
Priyanshu Kumar <priyanshukumarkota@gmail.com>
2025-06-21 23:24:15 +0530

    Experimenting

--- LeetCode/Sudoku_Solver_37.java
+++ LeetCode/Sudoku_Solver_37.java
@@ -1,64 +1,62 @@
-public class Sudoku_Solver_37 {
+import java.util.*;
 
-  public static void main(String[] args) {
-    int[][] board = {
-        { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
-        { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
-        { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
-        { 8, 0, 0, 0, 6, 0, 0, 0, 3 },
-        { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
-        { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
-        { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
-        { 0, 0, 0, 4, 1, 9, 0, 0, 5 },
-        { 0, 0, 0, 0, 8, 0, 0, 7, 9 }
-    };
+class Sudoku_Solver_37 {
 
-    if (solve(board)) {
-      printBoard(board);
-    } else {
-      System.out.println("No solution exists.");
-    }
-  }
+  public static boolean solveSudoku(char[][] board) {
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+        if (board[i][j] == '.') {
 
-  public static boolean solve(int[][] board) {
-    for (int row = 0; row < 9; row++) {
-      for (int col = 0; col < 9; col++) {
-        if (board[row][col] == 0) { // Empty cell
-          for (int num = 1; num <= 9; num++) {
-            if (isValid(board, row, col, num)) {
-              board[row][col] = num;
-              if (solve(board))
+          for (char c = '1'; c <= '9'; c++) {
+            if (isValid(board, i, j, c)) {
+              board[i][j] = c;
+
+              if (solveSudoku(board))
                 return true;
-              board[row][col] = 0; // Backtrack
+              else
+                board[i][j] = '.';
             }
           }
-          return false; // No valid number found
+
+          return false;
         }
       }
     }
-    return true; // All cells filled
+    return true;
   }
 
-  public static boolean isValid(int[][] board, int row, int col, int num) {
+  public static boolean isValid(char[][] board, int row, int col, char c) {
     for (int i = 0; i < 9; i++) {
-      // Check row and column
-      if (board[row][i] == num || board[i][col] == num)
+      if (board[i][col] == c)
         return false;
 
-      // Check 3x3 box
-      int boxRow = 3 * (row / 3) + i / 3;
-      int boxCol = 3 * (col / 3) + i % 3;
-      if (board[boxRow][boxCol] == num)
+      if (board[row][i] == c)
+        return false;
+
+      if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
         return false;
     }
     return true;
   }
 
-  public static void printBoard(int[][] board) {
-    for (int[] row : board) {
-      for (int num : row) {
-        System.out.print(num + " ");
-      }
+  public static void main(String[] args) {
+
+    char[][] board = {
+        { '9', '5', '7', '.', '1', '3', '.', '8', '4' },
+        { '4', '8', '3', '.', '5', '7', '1', '.', '6' },
+        { '.', '1', '2', '.', '4', '9', '5', '3', '7' },
+        { '1', '7', '.', '3', '.', '4', '9', '.', '2' },
+        { '5', '.', '4', '9', '7', '.', '3', '6', '.' },
+        { '3', '.', '9', '5', '.', '8', '7', '.', '1' },
+        { '8', '4', '5', '7', '9', '.', '6', '1', '3' },
+        { '.', '9', '1', '.', '3', '6', '.', '7', '5' },
+        { '7', '.', '6', '1', '8', '5', '4', '.', '9' }
+    };
+    solveSudoku(board);
+
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++)
+        System.out.print(board[i][j] + " ");
       System.out.println();
     }
   }
