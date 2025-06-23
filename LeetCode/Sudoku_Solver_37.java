3f7979044216910879a64f1e2266e733cb45b84e
Priyanshu Kumar <priyanshukumarkota@gmail.com>
2025-06-23 19:47:19 +0530

    Leetcode 37 Sudoku solver using Recursion

--- LeetCode/Sudoku_Solver_37.java
+++ LeetCode/Sudoku_Solver_37.java
@@ -1,46 +1,39 @@
-import java.util.*;
+import java.util.Arrays;
 
-class Sudoku_Solver_37 {
-
-  public static boolean solveSudoku(char[][] board) {
-    for (int i = 0; i < 9; i++) {
-      for (int j = 0; j < 9; j++) {
+public class Sudoku_Solver_37 {
+  public static boolean solveBoard(char[][] board) {
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[0].length; j++) {
         if (board[i][j] == '.') {
-
           for (char c = '1'; c <= '9'; c++) {
-            if (isValid(board, i, j, c)) {
+            if (isSafe(board, i, j, c)) {
               board[i][j] = c;
-
-              if (solveSudoku(board))
+              if (solveBoard(board))
                 return true;
               else
-                board[i][j] = '.';
+                board[i][j] = '.';// Backtrack
             }
           }
-
-          return false;
+          return false; // If no number can be placed, return false
         }
       }
     }
-    return true;
+    return true; // If all cells are filled correctly
   }
 
-  public static boolean isValid(char[][] board, int row, int col, char c) {
-    for (int i = 0; i < 9; i++) {
+  public static boolean isSafe(char[][] board, int row, int col, char c) {
+    for (int i = 0; i < board.length; i++) {
       if (board[i][col] == c)
-        return false;
-
+        return false; // Check column
       if (board[row][i] == c)
-        return false;
-
+        return false; // Check row
       if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
-        return false;
+        return false; // Check 3x3 subgrid
     }
-    return true;
+    return true; // If no conflicts found
   }
 
   public static void main(String[] args) {
-
     char[][] board = {
         { '9', '5', '7', '.', '1', '3', '.', '8', '4' },
         { '4', '8', '3', '.', '5', '7', '1', '.', '6' },
@@ -52,11 +45,15 @@ class Sudoku_Solver_37 {
         { '.', '9', '1', '.', '3', '6', '.', '7', '5' },
         { '7', '.', '6', '1', '8', '5', '4', '.', '9' }
     };
-    solveSudoku(board);
-
-    for (int i = 0; i < 9; i++) {
-      for (int j = 0; j < 9; j++)
-        System.out.print(board[i][j] + " ");
+    solveBoard(board);
+    // System.out.print(board); can't do it directly for 2D arrays will give random
+    // character adrress
+    // System.out.print(Arrays.deepToString(board)); do this instead to print 2D
+    // array
+    for (char[] row : board) {
+      for (char cell : row) {
+        System.out.print(cell + " ");
+      }
       System.out.println();
     }
   }
